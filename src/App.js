9fec371c218d5c16609056a03bf181fd505dc20a
HimanshiNawani <himanshinawani5@gmail.com>
2022-01-21 23:19:32 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState} from "react";
-
+import './style.css';
 import Comp from './Comp';
+
 export default function App() {
   const [flag, setFlag] =useState(true);
   return (
     <>
-
     <button onClick ={() => setFlag(!flag)}> Switch</button>
     {flag && <Comp/>}
     </>
